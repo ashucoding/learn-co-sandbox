311a3c6bf2b740a325849f2ff71c34e8079a3e06
Shana Wiltshire <shanawiltshire@gmail.com>
2020-04-24 21:27:47 +0000

    updated

--- trending_foods/lib/trending_foods.rb
+++ trending_foods/lib/trending_foods.rb
@@ -1,5 +1,5 @@
-require "../trending_foods/version"
-
+require_relative "./trending_foods/version"
+require_relative "./trending_foods/cli"
 module TrendingFoods
   class Error < StandardError; end
   # Your code goes here...
